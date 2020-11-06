e7723e93d04bd8c89e276727a12deac275c29e49
Yi Lian <yi.lian@mail.mcgill.ca>
2020-11-06 10:03:08 -0800

    Lecture 10 post-class

--- Lecture10.r
+++ Lecture10.r
@@ -11,6 +11,8 @@ apply(myarray, MARGIN = 1, FUN = sum)
 # What happened?
 apply(myarray, MARGIN = c(1, 3), FUN = sum)
 
+apply(myarray, MARGIN = c(2, 3), FUN = sum)
+
 set.seed(613)
 scores <- round(runif(n = 20, min = 50, max = 100)); scores
 
@@ -28,22 +30,26 @@ score.list
 # Average for each letter grade
 lapply(score.list, FUN = mean)
 
+mean(score.list$A)
+
+mean(score.list$A, na.rm = T)
+
 lapply(score.list, FUN = mean, na.rm = T)
 
 # sapply on a list
-sapply(score.list, FUN = mean)
+print(sapply(score.list, FUN = mean, na.rm = T))
 
 sapply(score.list, FUN = mean, na.rm = T, simplify = F)
 
 # sapply() on a data frame
 head(iris)
-sapply(iris, FUN = class)
+print(sapply(iris, FUN = class, simplify = T))
 
 # sapply on a vector
 sapply(c(6, 1, 3), FUN = seq)
 
 # sapply an operator instead of a function
-sapply(c(6, 1, 3), FUN = '+', 2)
+sapply(c(6, 1, 3), FUN = '%in%', 4:6)
 
 # use our own function in FUN
 sapply(c(6, 1, 3), FUN = function(x) {x + 2})
@@ -53,15 +59,25 @@ plyr::ldply(`.data` = c(6, 1, 3), `.fun` = function(x) {x + 2})
 
 score.list
 
+# summary.score <- score.list %>%
 
+plyr::ldply(`.data` = score.list, `.fun` = length)
+
+head(iris)
+
+iris$petalcat <- cut(iris$Petal.Width, c(0.0,0.1,0.2, Inf))
+
+head(iris)
 
 # recall aggregate()
-aggregate(Sepal.Length~Species, FUN = mean, data = iris)
+aggregate(Sepal.Length~Species+petalcat, FUN = mean, data = iris)
 
 # Alternative syntax
-aggregate(iris$Sepal.Length, by = list(Species = iris$Species), FUN = mean)
+aggregate(iris$Sepal.Length, by = list(Species = iris$Species,
+                                       Petal.width.category = iris$petalcat),
+          FUN = mean)
 
-tapply(X = iris$Sepal.Length, INDEX = list(Species = iris$Species), FUN = mean)
+tapply(X = iris$Sepal.Length, INDEX = list(Species = iris$Species, iris$petalcat), FUN = mean)
 
 # Another function that does the same thing
 by(data = iris$Sepal.Length, INDICES = list(Species = iris$Species), FUN = mean)
@@ -85,14 +101,20 @@ number.samples <- 25
 
 # Draw 25 samples of size 16 each and calculate 80%, 96% t-based confidence intervals
 mu <- mean(yrbss$weight)
-R.16.80.96 <- replicate(number.samples, {
+
+R.16.80.96 <- replicate(number.samples, expr = {
     sample.16 <- yrbss %>%
         dplyr::sample_n(size = 16) %>%
         dplyr::pull(weight)
+#         cat("sample", sample.16, "\n")
     
     c(t.test(sample.16, conf.level = .80)$conf.int,
       t.test(sample.16, conf.level = .96)$conf.int)
-}) %>% t()
+}
+                       ) %>% t()
+R.16.80.96
+
+mu
 
 # check coverage for 80% interval
 mean(sapply(1:number.samples,
@@ -101,6 +123,11 @@ mean(sapply(1:number.samples,
             }
     )
 )
+
+ttest <- t.test(1:10, conf.level = 0.75)
+str(ttest)
+
+ttest$conf.int
 
 # check coverage for 96% interval
 mean(sapply(1:number.samples, function(i){
@@ -127,15 +154,31 @@ abline(v = mu, lty = 2, col = "red")
 set.seed(12345)
 true_lambda <- 6
 sims <- rpois(100, lambda = true_lambda)
+sims
+
+poisson.test(x = 7, T = 1)
+
+ci_pois <- function(i) {
+    z_ci <- poisson.test(x = i, T = 1)$conf.int
+    c(z_ci, i, dplyr::between(true_lambda, z_ci[1],z_ci[2]))
+}
+
+ci_pois(12)
+
+# plyr::ldply
+plyr::ldply(`.data` = c(6, 1, 3), `.fun` = function(x) {x + 2})
+
 df <- plyr::ldply(sims, function(i) {
     z_ci <- poisson.test(x = i, T = 1)$conf.int
     c(z_ci,i, dplyr::between(true_lambda, z_ci[1],z_ci[2]))
 })
+df
 
 df$trial <- factor(seq_along(df$V1))
 colnames(df) <- c("lower","upper","mean", "covered", "trial")
 df$covered <- factor(df$covered, levels = 0:1, labels = c("no","yes"))
 df$width <- df$upper - df$lower
+df
 
 p <- ggplot(df, aes(mean, trial, colour = covered))
 p + geom_point() +
